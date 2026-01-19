84a8e9713fc8b788539e976ada5870e40ebb410b
utsakumar <kumarutsa2003@gmail.com>
2026-01-19 11:26:51 +0600

    v2.9

--- controller/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();          // Start the session
-session_unset();          // Clear all session variables
-session_destroy();        // Destroy the session
-
-// Redirect to login page
-header("Location: ../view/login.html?message=loggedout");
-exit;
-?>
